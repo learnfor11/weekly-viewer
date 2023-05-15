e95986f16fef88243d99b4eaac530459b1c4fc82
ppz <372301467@qq.com>
2023-05-15 18:29:30 +0800

    feat: catalog

--- app/page/home/index.js
+++ app/page/home/index.js
@@ -1,16 +1,12 @@
 import { useState } from 'react'
 import { $ } from '@ppzp/utils.rc'
+import { useQuery } from 'router-mini/react.js'
+
+import Catalog from './catalog.js'
 import Viewer from './viewer.js'
 
 export default
 function HomePage() {
-  const [num] = useState(function get_num_from_href() {
-    const params = new URLSearchParams(location.search)
-    const num = params.get('num')
-    if(!num)
-      throw Error('Missing num in query string')
-    return num
-  })
-  
-  return num && $(Viewer, { num })
+  const num = useQuery()?.num
+  return num ? $(Viewer, { num }) : $(Catalog)
 }
