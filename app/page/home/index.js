593e0c517aa66908ac08265a0cd82ed66e5d05ac
ppz <372301467@qq.com>
2023-05-15 16:23:15 +0800

    feat: matcher

--- app/page/home/index.js
+++ app/page/home/index.js
@@ -1,6 +1,26 @@
 import { $ } from '@ppzp/utils.rc'
 
+import { get_by_num } from '../../api/raw.js'
+import useAsyncGet from '../../common/use_async_get.js'
+
 export default
 function HomePage() {
-  return $.div('home')
+  const raw_res = useAsyncGet({
+    getter() {
+      const params = new URLSearchParams(location.search)
+      const num = params.get('num')
+      if(!num)
+        throw Error('Missing num in query string')
+      return get_by_num(num)
+    }
+  })
+  
+  return raw_res.match({
+    before_loaded: () => $.div('loading...'),
+    loaded: data =>
+      $.div(data)
+    ,
+    error: err =>
+      $.div(err)
+  })
 }
