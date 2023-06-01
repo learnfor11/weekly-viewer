154f7793bd27d226ac26653225528673e63a7338
ppz <372301467@qq.com>
2023-06-01 18:37:43 +0800

    feat: header

--- app/section/header.js
+++ app/section/header.js
@@ -1,8 +1,19 @@
 import { O } from '@ppzp/utils.rc'
 
 export default
-function Header() {
+function Header({ h1 }) {
   return O.header(
-    
+    O.h1(h1),
+
+    O.div({ className: 'links' },
+      O.a({
+        href: 'https://github.com/learnfor11/weekly_viewer',
+        target: '_blank'
+      }, 'Github'),
+      O.a({
+        href: 'http://www.ruanyifeng.com/blog/',
+        target: '_blank'
+      }, '阮一峰的网络日志')
+    )
   )
 }
