467d9ddf93058df17ba494c4596609d121c6924c
ppz <372301467@qq.com>
2023-07-11 15:43:16 +0800

    perf: 左侧 nav

--- app/section/catalog.js
+++ app/section/catalog.js
@@ -11,11 +11,6 @@ function Catalog() {
   const list = useState_list().value
   const current_article = useCurrent()
 
-  // 期号最长长度
-  const pad_size = useMemo(
-    () => list.slice(-1)[0].number.toString().length,
-    [list]
-  )
   // 按年月分组的 list
   const list2 = useMemo(group_list(list), [list])
 
@@ -23,7 +18,7 @@ function Catalog() {
     R('ul', { className: 'nav_wrapper' },
       list2.map(item =>
         YearGroup({
-          item, current_article, pad_size
+          item, current_article
         })
       )
     )
@@ -31,19 +26,30 @@ function Catalog() {
 }
 
 // 一年的分组
-function YearGroup({ item, current_article, pad_size }) {
+function YearGroup({ item, current_article }) {
   const expand = useExpand(false)
   return R('li', { key: item.label },
-    R('label', { onClick: expand.toggle }, item.label),
+    R('div',
+      {
+        className: 'group_label',
+        onClick: expand.toggle
+      },
+      item.label + ' 年',
+      R('span', { className: 'number' },
+        item.list.slice(-1)[0].list.slice(-1)[0].pad_num, // 最后一个月的，最后一期的，期号
+        ' ~ ',
+        item.list[0].list[0].pad_num // 第一个月的，第一期的，期号
+      )
+    ),
     R(Drawer,
       {
         y: true,
         expand: expand.expand
       },
-      R('ul', null,
+      R('ul', { className: 'number' },
         item.list.map(item =>
           MonthGroup({
-            item, current_article, pad_size
+            item, current_article
           })
         )
       )
@@ -52,10 +58,21 @@ function YearGroup({ item, current_article, pad_size }) {
 }
 
 // 一月的分组
-function MonthGroup({ item, current_article, pad_size }) {
+function MonthGroup({ item, current_article }) {
   const expand = useExpand(false)
   return R('li', { key: item.label },
-    R('label', { onClick: expand.toggle }, item.label),
+    R('div',
+      {
+        className: 'group_label',
+        onClick: expand.toggle
+      },
+      item.label.toString().padStart(2, '0') + ' 月',
+      R('span', { className: 'number' },
+        item.list.slice(-1)[0].pad_num,
+        ' ~ ',
+        item.list[0].pad_num
+      )
+    ),
     R(Drawer,
       {
         y: true,
@@ -64,15 +81,14 @@ function MonthGroup({ item, current_article, pad_size }) {
       R('ul', null,
         item.list.map(item => {
           // 渲染某一期
-          const padded_num = item.number.toString().padStart(pad_size, '0')
           return R('li', { key: item.number },
             R(Link,
               {
                 disabled: item.number == current_article?.number,
                 to: item.number,
-                title: `第 ${padded_num} 期 ${item.title || '（第 106 期之前没标题）' }`,
+                title: `第 ${item.pad_num} 期 ${item.title || '（第 106 期之前没标题）' }`,
               },
-              R('span', { className: 'number' }, padded_num, '. '),
+              R('span', { className: 'number' }, item.pad_num, '. '),
               item.title || R('span', { className: 'no_name' }, '无标题')
             )
           )
@@ -83,18 +99,18 @@ function MonthGroup({ item, current_article, pad_size }) {
 }
 
 const group_list = list => () => {
+  const pad_size = list[0].number.toString().length
   const result = []
   for(let item of list) {
+    item.pad_num = item.number.toString().padStart(pad_size, '0')
     const year = item.date.getFullYear()
     const month = item.date.getMonth() + 1
     
     const last_year = result[result.length - 1]
     const current_year = last_year?.label == year ? last_year : p_r(result, {
       label: year,
-      amount: 0, // 统计篇数
       list: []
     })
-    current_year.amount++
 
     const last_month = current_year.list.slice(-1)[0]
     const current_month = last_month?.label == month ? last_month : p_r(current_year.list, {
